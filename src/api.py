f54c33aaa7548c890078095e1c1c45bea2ba0d96
MohamedRachadi <mohamed.rachadi.etu@univ-lille.fr>
2025-01-13 19:54:54 +0100

    api.py changes

--- src/api.py
+++ src/api.py
@@ -3,14 +3,20 @@ from pydantic import BaseModel
 import mlflow
 import pandas as pd
 
+import os
+
+# Récupérer l'adresse de MLflow selon l'environnement
+tracking_uri = "http://127.0.0.1:5000"  # En local
+if os.getenv("DOCKER", "false") == "true":
+    tracking_uri = "http://host.docker.internal:5000"  # Dans Docker
+
 # Création de l'application FastAPI
 app = FastAPI()
 
 # Charger le modèle depuis MLflow
 model = None
 try:
-    #mlflow.set_tracking_uri("http://host.docker.internal:5000")
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_tracking_uri(tracking_uri)
     model_name = "Best_Model_RandomForestRegressor"
     model = mlflow.sklearn.load_model(f"models:/{model_name}/latest")
 except Exception as e:
