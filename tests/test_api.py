433a1840a282685d04107f5dcba420cc86f954a0
MohamedRachadi <mohamed.rachadi.etu@univ-lille.fr>
2025-01-13 23:16:41 +0100

    Test Pipelinie 2.21

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,17 +1,33 @@
-import requests
+import pytest
+from fastapi.testclient import TestClient
+from src.api import app
+
+client = TestClient(app)
+
+def test_predict_invalid_data():
+    response = client.post("/predict", json={
+        "MedInc": "string",  # Mauvais type de donnée
+        "HouseAge": 30, 
+        "AveRooms": 6.5, 
+        "AveBedrms": 2.0, 
+        "Population": 1000, 
+        "AveOccup": 3.0, 
+        "Latitude": 34.5, 
+        "Longitude": -118.5
+    })
+    assert response.status_code == 422  # Unprocessable Entity (erreur validation)
+
 
 def test_predict():
-    data = {
-        "MedInc": 3.2,
-        "HouseAge": 30,
-        "AveRooms": 6.5,
-        "AveBedrms": 2.0,
-        "Population": 1000,
-        "AveOccup": 3.0,
-        "Latitude": 34.5,
+    response = client.post("/predict", json={
+        "MedInc": 3.2, 
+        "HouseAge": 30, 
+        "AveRooms": 6.5, 
+        "AveBedrms": 2.0, 
+        "Population": 1000, 
+        "AveOccup": 3.0, 
+        "Latitude": 34.5, 
         "Longitude": -118.5
-    }
-    #response = requests.post("http://host.docker.internal:8000/predict", json=data)
-    response = requests.post("http://localhost:8000/predict", json=data)
+    })
     assert response.status_code == 200
     assert "prediction" in response.json()
