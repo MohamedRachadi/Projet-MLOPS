e0b3f53d77cdba914bc3b432e7a50b65e4bc43f7
MohamedRachadi <mohamed.rachadi.etu@univ-lille.fr>
2025-01-14 21:08:20 +0100

    Test Pipeline 3.17

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,11 @@
 import requests
+import os
 
 def test_model_serving():
     # L'URL de votre API MLflow servie
-    url = "http://localhost:5000/predict"  # Utilisation de /predict
+    #url = "http://localhost:5000/predict"  # Utilisation de /predict
     #url = "http://host.docker.internal:5000/predict"
+    url = "http://localhost:5000/predict" if os.getenv("DOCKER", "false") == "false" else "http://host.docker.internal:5000/predict"
 
     # Données d'entrée pour tester le modèle (ici, un exemple basé sur le jeu de données California Housing)
     test_data = {
